2e44c781f80d6988a53b9e9c7203115de1705718
Kaylin <kzaroukian@gmail.com>
2018-10-16 15:26:04 -0400

    ARP and ICMP working

--- route2.c
+++ route2.c
@@ -57,13 +57,6 @@ int main() {
     u_int16_t checksum;
   };
 
-  //get list of interface addresses. This is a linked list. Next
-  //pointer is in ifa_next, interface name is in ifa_name, address is
-  //in ifa_addr. You will have multiple entries in the list with the
-  //same name, if the same interface has multiple addresses. This is
-  //common since most interfaces will have a MAC, IPv4, and IPv6
-  //address. You can use the names to match up which IPv4 address goes
-  //with which MAC address.
   struct ifaddrs *ifaddr, *tmp;
   if(getifaddrs(&ifaddr)==-1){
     perror("getifaddrs");
@@ -73,11 +66,7 @@ int main() {
   int i = 0;
   for(tmp = ifaddr; tmp!=NULL; tmp=tmp->ifa_next){
     i++;
-    //Check if this is a packet address, there will be one per
-    //interface.  There are IPv4 and IPv6 as well, but we don't care
-    //about those for the purpose of enumerating interfaces. We can
-    //use the AF_INET addresses in this list for example to get a list
-    //of our own IP addresses
+
     if(tmp->ifa_addr->sa_family==AF_PACKET){
       printf("Interface: %s\n",tmp->ifa_name);
 
@@ -85,13 +74,6 @@ int main() {
       //create a packet socket on interface r?-eth1
       if(!strncmp(&(tmp->ifa_name[3]),"eth1",4)){
         printf("Creating Socket on interface %s\n",tmp->ifa_name);
-
-        //create a packet socket
-        //AF_PACKET makes it a packet socket
-        //SOCK_RAW makes it so we get the entire packet
-        //could also use SOCK_DGRAM to cut off link layer header
-        //ETH_P_ALL indicates we want all (upper layer) protocols
-        //we could specify just a specific one
 
         struct sockaddr_ll *r_mac_addr = (struct sockaddr_ll *)tmp->ifa_addr;
         memcpy(router_mac_addr, r_mac_addr->sll_addr, 6);
@@ -103,12 +85,6 @@ int main() {
           return 2;
         }
 
-        //Bind the socket to the address, so we only get packets
-        //recieved on this specific interface. For packet sockets, the
-        //address structure is a struct sockaddr_ll (see the man page
-        //for "packet"), but of course bind takes a struct sockaddr.
-        //Here, we can use the sockaddr we got from getifaddrs (which
-        //we could convert to sockaddr_ll if we needed to)
         if(bind(packet_socket,tmp->ifa_addr,sizeof(struct sockaddr_ll))==-1){
           perror("bind");
         }
@@ -118,12 +94,6 @@ int main() {
       if(!strncmp(&(tmp->ifa_name[2]),"lo",2)){
         printf("Creating Socket on interface %s\n",tmp->ifa_name);
 
-        //create a packet socket
-        //AF_PACKET makes it a packet socket
-        //SOCK_RAW makes it so we get the entire packet
-        //could also use SOCK_DGRAM to cut off link layer header
-        //ETH_P_ALL indicates we want all (upper layer) protocols
-        //we could specify just a specific one
         lo_socket = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
 
         if(lo_socket<0){
@@ -131,12 +101,6 @@ int main() {
           return 2;
         }
 
-        //Bind the socket to the address, so we only get packets
-        //recieved on this specific interface. For packet sockets, the
-        //address structure is a struct sockaddr_ll (see the man page
-        //for "packet"), but of course bind takes a struct sockaddr.
-        //Here, we can use the sockaddr we got from getifaddrs (which
-        //we could convert to sockaddr_ll if we needed to)
         if(bind(lo_socket,tmp->ifa_addr,sizeof(struct sockaddr_ll))==-1){
           perror("bind");
         }
@@ -146,12 +110,6 @@ int main() {
       if(!strncmp(&(tmp->ifa_name[3]),"eth0",4)){
         printf("Creating Socket on interface %s\n",tmp->ifa_name);
 
-        //create a packet socket
-        //AF_PACKET makes it a packet socket
-        //SOCK_RAW makes it so we get the entire packet
-        //could also use SOCK_DGRAM to cut off link layer header
-        //ETH_P_ALL indicates we want all (upper layer) protocols
-        //we could specify just a specific one
         eth0_socket = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
 
         if(eth0_socket<0){
@@ -159,12 +117,7 @@ int main() {
           return 2;
         }
 
-        //Bind the socket to the address, so we only get packets
-        //recieved on this specific interface. For packet sockets, the
-        //address structure is a struct sockaddr_ll (see the man page
-        //for "packet"), but of course bind takes a struct sockaddr.
-        //Here, we can use the sockaddr we got from getifaddrs (which
-        //we could convert to sockaddr_ll if we needed to)
+
         if(bind(eth0_socket,tmp->ifa_addr,sizeof(struct sockaddr_ll))==-1){
           perror("bind");
         }
@@ -174,12 +127,6 @@ int main() {
           if(!strncmp(&(tmp->ifa_name[3]),"eth2",4)){
           printf("Creating Socket on interface %s\n",tmp->ifa_name);
 
-          //create a packet socket
-          //AF_PACKET makes it a packet socket
-          //SOCK_RAW makes it so we get the entire packet
-          //could also use SOCK_DGRAM to cut off link layer header
-          //ETH_P_ALL indicates we want all (upper layer) protocols
-          //we could specify just a specific one
           eth1_socket = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
 
           if(eth1_socket<0){
@@ -187,12 +134,6 @@ int main() {
             return 2;
           }
 
-            //Bind the socket to the address, so we only get packets
-            //recieved on this specific interface. For packet sockets, the
-            //address structure is a struct sockaddr_ll (see the man page
-            //for "packet"), but of course bind takes a struct sockaddr.
-            //Here, we can use the sockaddr we got from getifaddrs (which
-            //we could convert to sockaddr_ll if we needed to)
             if(bind(eth1_socket,tmp->ifa_addr,sizeof(struct sockaddr_ll))==-1){
               perror("bind");
             }
@@ -205,13 +146,6 @@ int main() {
   FD_SET(lo_socket, &sockets);
   FD_SET(eth0_socket, &sockets);
   FD_SET(eth1_socket, &sockets);
-  //
-  // typedef struct {
-  //   uint16_t type;
-  //   uint16_t code;
-  //   uint16_t checksum;
-  // } icmp_header;
-
 
   printf("Ready to recieve now\n");
   while(1){
@@ -245,6 +179,7 @@ int main() {
           struct ether_arp *arp_request = (struct ether_arp*)(buf+14);
 
           char reply_data[1500];
+          memcpy(reply_data,buf,1500);
 
           struct ether_header *eth_reply = (struct ether_header*)reply_data;
           //struct arp_header *arp_reply = (struct arp_header*)(reply_data+14);
@@ -260,6 +195,10 @@ int main() {
           printf("Starting arp_reply\n");
 
           memcpy(&(arp_reply->ea_hdr), &(arp_request->ea_hdr), sizeof(arp_request->ea_hdr));
+          //memcpy(&(arp_reply->arp_op), ARPOP_REPLY, sizeof(arp_request->ea_hdr.ea_hdr));
+          arp_reply->arp_op = ntohs(ARPOP_REPLY);
+          // this may be wrong but double check
+          //arp_reply->ea_hdr.arp_op=ARPOP_REPLY;
           memcpy(&(arp_reply->arp_sha), router_mac_addr, 6);
           memcpy(&(arp_reply->arp_spa), &(arp_request->arp_tpa), 4);
           memcpy(&(arp_reply->arp_tha), &(arp_request->arp_sha), 6);
@@ -276,7 +215,7 @@ int main() {
           printf("ARP Reply packet sent\n");
 
         }
-        if (ntohs(eth_request->ether_type) == 2048){
+        if (ntohs(eth_request->ether_type) == ETHERTYPE_IP){
           // header for icmp request
           printf("ICMP Request\n");
           struct iphdr *ip_request = (struct iphdr*)(buf+sizeof(struct ether_header));
@@ -287,26 +226,19 @@ int main() {
           printf("%d\n", sizeof(struct icmphdr));
 
           // replies for ICMP
-          char reply_data[1500];
+          char reply_data[1514];
+          memcpy(reply_data, buf, 1514);
+
           struct ether_header *eth_reply = (struct ether_header*)reply_data;
           struct iphdr *ip_reply = (struct iphdr*)(reply_data+sizeof(struct ether_header));
           // struct icmphdr *icmp_reply = (struct icmphdr*)(reply_data + 14+20);
           u_short ip2_len = ip_reply->ihl * 4;
           struct icmp_header *icmp_reply = (struct icmp_header*)(reply_data + 14+20);
 
-
           printf("Reply header \n");
 
-          // printf("OG ETH DST MAC: %d:%d:%d:%d:%d:%d\n",eth_request->ether_dhost[0], eth_request->ether_dhost[1], eth_request->ether_dhost[2], eth_request->ether_dhost[3], eth_request->ether_dhost[4],eth_request->ether_dhost[5]);
-          // printf("OG ETH SRC MAC: %d:%d:%d:%d:%d:%d\n",eth_request->ether_shost[0], eth_request->ether_shost[1], eth_request->ether_shost[2], eth_request->ether_shost[3], eth_request->ether_shost[4],eth_request->ether_shost[5]);
-          //
-          // printf("OG IP DST: %d.%d.%d.%d", ip_request->daddr[0],ip_request->daddr[1],ip_request->daddr[2], ip_request->daddr[3]);
-          // printf("OG IP SRC: %d.%d.%d.%d", ip_request->saddr[0],ip_request->saddr[1],ip_request->saddr[2], ip_request->saddr[3]);
-          // printf("OG ICMP HDR TYPE %d\n", icmp_request->type);
-          // printf("OG ICMP HDR CODE %d\n", icmp_request->code);
-          // printf("OG ICMP HDR CHKSUM %d\n", icmp_request->checksum);
-
           //populates the ethernet header
+          memcpy(&(eth_reply),&(eth_request),20);
           memcpy(&(eth_reply->ether_dhost),&(eth_request->ether_shost),6);
           memcpy(&(eth_reply->ether_shost), &(eth_request->ether_dhost),6);
           memcpy(&(eth_reply->ether_type), &(eth_request->ether_type), 2);
@@ -317,36 +249,13 @@ int main() {
           memcpy(&(ip_reply->daddr), &(ip_request->saddr), 4);
           printf("IP header\n");
 
-
-          // struct icmp_header {
-          //   u_int8_t type;
-          //   u_int8_t code;
-          //   u_int16_t checksum;
-          // }
           // populates the icmp headers
           icmp_reply->type = 0;
           icmp_reply->code = 0;
-          uint8_t ans = 0;
-          // memcpy(&(icmp_reply->type), ans, 1);
-          // memcpy(&(icmp_reply->code), ans, 1);
           memcpy(&(icmp_reply->checksum), &(ip_request->check), 2);
           printf("ICMP hdr\n");
 
-          // printf("TYPE: %d\n", icmp_reply->type);
-          // printf("CODE: %d\n", icmp_reply->code);
-          // printf("CS: %d\n", icmp_reply->checksum);
-          // printf("REPLY: %s\n", reply_data);
-          //
-          // printf("NEW ETH DST MAC: %d:%d:%d:%d:%d:%d\n",eth_reply->ether_dhost[0], eth_reply->ether_dhost[1], eth_reply->ether_dhost[2], eth_reply->ether_dhost[3], eth_reply->ether_dhost[4],eth_reply->ether_dhost[5]);
-          // printf("NEW ETH SRC MAC: %d:%d:%d:%d:%d:%d\n",eth_reply->ether_shost[0], eth_reply->ether_shost[1], eth_reply->ether_shost[2], eth_reply->ether_shost[3], eth_reply->ether_shost[4],eth_reply->ether_shost[5]);
-          //
-          // printf("NEW IP DST: %d.%d.%d.%d", ip_reply->daddr[0],ip_reply->daddr[1],ip_reply->daddr[2], ip_reply->daddr[3]);
-          // printf("NEW IP SRC: %d.%d.%d.%d", ip_reply->saddr[0],ip_reply->saddr[1],ip_reply->saddr[2], ip_reply->saddr[3]);
-          // printf("NEW ICMP HDR TYPE %d\n", icmp_reply->type);
-          // printf("NEW ICMP HDR CODE %d\n", icmp_reply->code);
-          // printf("NEW ICMP HDR CHKSUM %d\n", icmp_reply->checksum);
-
-          int x = send(i, reply_data, 74, 0);
+          int x = send(i, reply_data, 98, 0);
 
           if (x < 0) {
             printf("ERROR sending ICMP ECHO Reply!\n");
