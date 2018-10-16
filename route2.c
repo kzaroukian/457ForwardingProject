9a1f3ca5385b95e6738c0f0041363ca071c14b9d
Kaylin <kzaroukian@gmail.com>
2018-10-16 11:11:14 -0400

    working on ip

--- route2.c
+++ route2.c
@@ -9,6 +9,7 @@
 #include <netinet/ip.h>
 #include <netinet/if_ether.h>
 #include <arpa/inet.h>
+#include <netinet/ip_icmp.h>
 
 /**
  * @Author Kaylin Zaroukian, Jerry, Cody Krueger
@@ -49,6 +50,12 @@ int main() {
   // set of sockets
   fd_set sockets;
   FD_ZERO(&sockets);
+
+  struct icmp_header {
+    u_int8_t type;
+    u_int8_t code;
+    u_int16_t checksum;
+  };
 
   //get list of interface addresses. This is a linked list. Next
   //pointer is in ifa_next, interface name is in ifa_name, address is
@@ -227,16 +234,10 @@ int main() {
           continue;
         //start processing all others
         printf("Got a %d byte packet\n", n);
-
-        // header for arp
-        // not sure if we need both of these
-        // struct ether_arp *arp_hdr;
-        // arp_hdr = (struct ether_arp*)(buf + 14);
-
-        // we are receiving all arp packets
-        // checks to see which packets are arp
         struct ether_header *eth_request  = (struct ether_header*)buf;
+        printf("Ether type: %d\n", ntohs(eth_request->ether_type));
 
+        // checks if packet is arp
         if (ntohs(eth_request->ether_type) == ETH_P_ARP) {
           printf("ARP packet\n");
 
@@ -275,6 +276,88 @@ int main() {
           printf("ARP Reply packet sent\n");
 
         }
+        if (ntohs(eth_request->ether_type) == 2048){
+          // header for icmp request
+          printf("ICMP Request\n");
+          struct iphdr *ip_request = (struct iphdr*)(buf+sizeof(struct ether_header));
+          u_short ip_len = ip_request->ihl * 4;
+          // struct icmphdr *icmp_request = (struct icmphdr*)(buf + 34);
+          struct icmp_header *icmp_request = (struct icmp_header*)(buf + 14 + ip_len);
+          printf("Request Header created\n");
+          printf("%d\n", sizeof(struct icmphdr));
+
+          // replies for ICMP
+          char reply_data[1500];
+          struct ether_header *eth_reply = (struct ether_header*)reply_data;
+          struct iphdr *ip_reply = (struct iphdr*)(reply_data+sizeof(struct ether_header));
+          // struct icmphdr *icmp_reply = (struct icmphdr*)(reply_data + 14+20);
+          u_short ip2_len = ip_reply->ihl * 4;
+          struct icmp_header *icmp_reply = (struct icmp_header*)(reply_data + 14+20);
+
+
+          printf("Reply header \n");
+
+          // printf("OG ETH DST MAC: %d:%d:%d:%d:%d:%d\n",eth_request->ether_dhost[0], eth_request->ether_dhost[1], eth_request->ether_dhost[2], eth_request->ether_dhost[3], eth_request->ether_dhost[4],eth_request->ether_dhost[5]);
+          // printf("OG ETH SRC MAC: %d:%d:%d:%d:%d:%d\n",eth_request->ether_shost[0], eth_request->ether_shost[1], eth_request->ether_shost[2], eth_request->ether_shost[3], eth_request->ether_shost[4],eth_request->ether_shost[5]);
+          //
+          // printf("OG IP DST: %d.%d.%d.%d", ip_request->daddr[0],ip_request->daddr[1],ip_request->daddr[2], ip_request->daddr[3]);
+          // printf("OG IP SRC: %d.%d.%d.%d", ip_request->saddr[0],ip_request->saddr[1],ip_request->saddr[2], ip_request->saddr[3]);
+          // printf("OG ICMP HDR TYPE %d\n", icmp_request->type);
+          // printf("OG ICMP HDR CODE %d\n", icmp_request->code);
+          // printf("OG ICMP HDR CHKSUM %d\n", icmp_request->checksum);
+
+          //populates the ethernet header
+          memcpy(&(eth_reply->ether_dhost),&(eth_request->ether_shost),6);
+          memcpy(&(eth_reply->ether_shost), &(eth_request->ether_dhost),6);
+          memcpy(&(eth_reply->ether_type), &(eth_request->ether_type), 2);
+          printf("Eth header\n");
+
+          // populates the ip header
+          memcpy(&(ip_reply->saddr), &(ip_request->daddr), 4);
+          memcpy(&(ip_reply->daddr), &(ip_request->saddr), 4);
+          printf("IP header\n");
+
+
+          // struct icmp_header {
+          //   u_int8_t type;
+          //   u_int8_t code;
+          //   u_int16_t checksum;
+          // }
+          // populates the icmp headers
+          icmp_reply->type = 0;
+          icmp_reply->code = 0;
+          uint8_t ans = 0;
+          // memcpy(&(icmp_reply->type), ans, 1);
+          // memcpy(&(icmp_reply->code), ans, 1);
+          memcpy(&(icmp_reply->checksum), &(ip_request->check), 2);
+          printf("ICMP hdr\n");
+
+          // printf("TYPE: %d\n", icmp_reply->type);
+          // printf("CODE: %d\n", icmp_reply->code);
+          // printf("CS: %d\n", icmp_reply->checksum);
+          // printf("REPLY: %s\n", reply_data);
+          //
+          // printf("NEW ETH DST MAC: %d:%d:%d:%d:%d:%d\n",eth_reply->ether_dhost[0], eth_reply->ether_dhost[1], eth_reply->ether_dhost[2], eth_reply->ether_dhost[3], eth_reply->ether_dhost[4],eth_reply->ether_dhost[5]);
+          // printf("NEW ETH SRC MAC: %d:%d:%d:%d:%d:%d\n",eth_reply->ether_shost[0], eth_reply->ether_shost[1], eth_reply->ether_shost[2], eth_reply->ether_shost[3], eth_reply->ether_shost[4],eth_reply->ether_shost[5]);
+          //
+          // printf("NEW IP DST: %d.%d.%d.%d", ip_reply->daddr[0],ip_reply->daddr[1],ip_reply->daddr[2], ip_reply->daddr[3]);
+          // printf("NEW IP SRC: %d.%d.%d.%d", ip_reply->saddr[0],ip_reply->saddr[1],ip_reply->saddr[2], ip_reply->saddr[3]);
+          // printf("NEW ICMP HDR TYPE %d\n", icmp_reply->type);
+          // printf("NEW ICMP HDR CODE %d\n", icmp_reply->code);
+          // printf("NEW ICMP HDR CHKSUM %d\n", icmp_reply->checksum);
+
+          int x = send(i, reply_data, 74, 0);
+
+          if (x < 0) {
+            printf("ERROR sending ICMP ECHO Reply!\n");
+            perror("Error sending ICMP ECHO reply");
+            continue;
+          }
+
+          printf("ICMP Echo Reply packet sent\n");
+
+        }
+
       }
     }
     printf("Exiting for loop\n");
